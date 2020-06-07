6c25f69a190490333e55d6566e592eb1ac6ffd82
David <david.joech@gmail.com>
2020-06-07 10:13:36 +0200

    LaunchDetailView styling tweaks

--- SpaceWatch/Views/launch/LaunchDetailView.swift
+++ SpaceWatch/Views/launch/LaunchDetailView.swift
@@ -10,8 +10,21 @@ import SwiftUI
 
 struct LaunchDetailView: View {
     let launch: Launch
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(launch.mission_name)
+                .font(.title)
+                .bold()
+            Spacer()
+        }
+        .padding()
+        .navigationBarTitle(Text(""), displayMode: .inline)
     }
-    
+}
+
+struct LaunchDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchDetailView(launch: Launch(flight_number: 1, mission_name: "TestMission", launch_site: LaunchSite(site_name: "Test Site", site_name_long: "Test Site Long")))
+    }
 }
